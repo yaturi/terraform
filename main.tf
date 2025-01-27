5d50b7fa8c7b1176cf63db20ca869501cebff2b9
Ravi Prasad Reddy <ravi@Ravis-MacBook-Air.local>
2025-01-27 12:34:24 +0530

    provider added

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
 resource "aws_vpc" "vpc-terraform" {
     cidr_block = "10.0.0.0/16"
 
     tags = {
         Name = "terraform-vpc"
+    }
+  
+}
+
+resource "aws_internet_gateway" "IGW-terraform" {
+    vpc_id = aws_vpc.vpc-terraform.id
+
+    tags = {
+      Name = "main"
     }
   
 }
